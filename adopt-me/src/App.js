acea70c80106383cd564ba2651c65d0f6f153e2f
Nicholas Ellingson <nickellingson8@gmail.com>
2023-08-21 15:21:24 -0500

    Add base code and config

---
+++ adopt-me/src/App.js
@@ -0,0 +1,45 @@
+// Notes
+// One Way Data Flow, Parent -> Child, very explicit
+//
+//
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+//////////////
+// Before JSX
+const App = () => {
+  return React.createElement(
+    "div", // element, tag
+    {}, // attributes
+    [
+      React.createElement("h1", {}, "Adopt Me!"), // children
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese",
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel",
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "Cat",
+        breed: "Mixed",
+      }),
+    ]
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
+// Before JSX
+//////////////
