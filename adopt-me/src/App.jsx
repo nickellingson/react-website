078c4e88188abda9fc674376b1ef733136dc03a5
Nicholas Ellingson <nickellingson8@gmail.com>
2023-08-31 20:15:47 -0500

    add useParams

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -5,6 +5,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 
 //////////////
 // Before JSX
@@ -35,10 +37,15 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/"> Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
 
 // const Pet = (props) => {
 //     return React.createElement("div", {}, [
@@ -14,19 +15,17 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
 
   return (
-    <>
-      <a href={`/details/${id}`} className="pet">
-        <div className="image-container">
-          <img src={hero} alt={name} />
-        </div>
-        <div className="info">
-          <h1>{name}</h1>
-          <h2>
-            {animal} — {breed} — {location}
-          </h2>
-        </div>
-      </a>
-    </>
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} — {breed} — {location}
+        </h2>
+      </div>
+    </Link>
   );
 };
 
